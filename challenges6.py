3c7563c1bfa913db8b3a8c0fe4e3d0e1db2fe1c9
MariaMercedeslopez97 <167473751+MariaMercedeslopez97@users.noreply.github.com>
2024-06-18 17:56:21 -0400

    Add files via upload
    Conversión de Temperatura

---
+++ challenges6.py
@@ -0,0 +1,17 @@
+#DIA 5
+#Conversión de Temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a
+#  grados Fahrenheit.
+
+def temperatura(celsius):
+    #Formula para convertir Celsius a Fahrenheit
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+#Solicitar al usuario la temperatura en grados Celsius
+celsius = float(input("Introduce la temperatura en grados Celsius: "))
+
+#Realizar la conversion
+fahrenheit = temperatura(celsius)
+
+#Mostrar el resultado
+print(f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit")
